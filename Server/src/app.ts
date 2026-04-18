525dafb3c9e8afe6b14d9e22b6aae8dc9dd717b8
Agada Daniel <unogwuagada@gmail.com>
2026-04-18 17:57:26 +0100

    route change

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -10,12 +10,32 @@ import scannerAuthRoutes from "./routes/scannerAuthRoutes";
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//   }),
+// );
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://automated-attendance-taker.vercel.app",
+];
+
 app.use(
   cors({
-    origin: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  }),
-);
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
 app.use(express.json({ limit: "2mb" }));
 
 app.get("/health", (_req, res) => {
